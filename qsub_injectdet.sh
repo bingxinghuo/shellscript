8512614bce66415c0552ab78e1029b84d2d927e3
Bingxing Huo <bhuo@mitraimacpro-bh.cshl.edu>
2019-09-06 21:38:55 -0400

    added signal detection script

---
+++ qsub_injectdet.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#  qsub_procdet.sh
+#  
+#
+#  Created by Bingxing Huo on 8/30/19.
+#  
+# specify BASH shell
+#$ -S /bin/bash
+# run job in the current working directory where qsub is executed from
+#$ -cwd
+#  specify that the job requires 60GB of memory
+#$ -l m_mem_free=60G
+#$ -pe threads 10
+#$ -V
+# run commands and application
+pwd
+date
+matlab -nodesktop -r injectdet_bnb
+date
